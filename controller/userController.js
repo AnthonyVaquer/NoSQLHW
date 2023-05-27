6a4ed4d4b9249132f750b2971eb6a51ad30ecca1
Anthony Vaquer <anthonymark126@gmail.com>
2023-05-27 08:26:56 -0700

    updates

--- controller/userController.js
+++ controller/userController.js
@@ -33,5 +33,34 @@ module.exports = {
     } catch (err) {
       res.status(500).json(err);
     }
-  },
+  }
+
+  // Delete a user and remove them from the friend's list
+// async deleteUser(req, res) {
+//   try {
+//     const user = await User.findOneAndRemove({ _id: req.params.userId });
+
+//     if (!user) {
+//       return res.status(404).json({ message: 'This user does not exist' })
+//     }
+
+//     const friends = await Friends.findOneAndUpdate(
+//       { user: req.params.userId },
+//       { $pull: { friends: req.params.userId } },
+//       { new: true }
+//     );
+
+//     if (!course) {
+//       return res.status(404).json({
+//         message: 'User deleted',
+//       });
+//     }
+
+//     res.json({ message: 'User successfully deleted' });
+//   } catch (err) {
+//     console.log(err);
+//     res.status(500).json(err);
+//   }
+// }
 };
+
